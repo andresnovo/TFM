587e6e0ade055caa73393f74830eb7241c89833d
andresnovo <andresnovor@gmail.com>
2023-11-15 02:41:17 +0100

    PT_plots/hist_11114066.png

--- first/first.py
+++ first/first.py
@@ -8,17 +8,62 @@ import awkward as ak
 
 
 if __name__ == "__main__":
-    root = '/scratch47/forAndres/tuples/simulation/2016/'
-    f=ur.open(root +'11114077/StrippingB2KpiX2MuMuDarkBosonLine.root:B2KpiX2MuMuDarkBoson/DecayTree')
-    print(f.keys())
-    df=f.arrays(['piminus_P','Kplus_PE','Kplus_P','piminus_PE','Kplus_M', 'piminus_M','Kplus_PX','Kplus_PY','Kplus_PZ','piminus_PX','piminus_PY','piminus_PZ'],library='pd')
-    df.eval('kstar_m = (Kplus_M**2 + piminus_M**2 + 2*(Kplus_PE*piminus_PE - (Kplus_PX*piminus_PX + Kplus_PY*piminus_PY + Kplus_PZ*piminus_PZ)))**(1/2)',inplace=True)
-  
 
-    print(df['kstar_m'])
-    plt.hist(df['kstar_m'],bins=50)
-    plt.savefig('kstar_M.png')
-    
-   
-    
+    def files(path,p,m,t):
+        root = '/scratch47/forAndres/tuples/simulation/2016/'
+        if p==path_B0:
+            f=ur.open(root + path + '/StrippingB2KpiX2MuMuDarkBosonLine.root:B2KpiX2MuMuDarkBoson/DecayTree')
+            data=f.arrays(['piminus_PT','Kplus_PT','muminus_PT','muplus_PT'],library='pd')
+        else:
+            f=ur.open(root + path + '/StrippingB2KX2MuMuDarkBosonLine.root:B2KX2MuMuDarkBoson/DecayTree')
+            data=f.arrays(['Kplus_PT','muminus_PT','muplus_PT'],library='pd')
+        return data
 
+       
+    
+    path_B0=['11114066','11114074','11114077']; path_Bplus=['12113086','12113095','12113098']
+    m=[2000,250,4000]
+    t=[100,100,100]
+    
+    for i,j in enumerate(path_B0):
+        df=files(j,path_B0,m[i],t[i])
+
+        plt.figure()
+        plt.suptitle('Comparativa dos momentos transversais das partículas do decaemento\n $B^{0} \longrightarrow (K^{*0}_{(892)} \longrightarrow K^{+}~\pi^{-}) (H^{0} \longrightarrow \mu^{+}~\mu^{-}) \quad m=%d~MeV \quad t=%d~ps$' %(m[i],t[i]))
+        ax1=plt.subplot(1,2,1)
+        ax1.hist(df['piminus_PT'], bins=50, color='blue', label='$\pi^{-}_{PT}$')
+        ax1.hist(df['Kplus_PT'], bins=50, color='red', label='$K^{+}_{PT}$')
+        ax1.legend(loc='best')
+        ax1.set_xlabel('$Energy~(MeV)$')
+        ax1.set_ylabel('$Pseudorapidity$')
+        ax1.minorticks_on()
+        ax2=plt.subplot(1,2,2)
+        ax2.hist(df['muplus_PT'], bins=50, color='blue', label='$\mu^{+}_{PT}$')
+        ax2.hist(df['muminus_PT'], bins=50, color='red', label='$\mu^{-}_{PT}$')
+        ax2.legend(loc='best')
+        ax2.set_xlabel('$Energy~(MeV)$')
+        ax2.set_ylabel('$Pseudorapidity$')
+        ax2.minorticks_on()
+        plt.subplots_adjust(wspace=.5)
+        plt.savefig('/home3/andres.novo/TFM/first/PT_plots/hist_'+ j +'.png')
+
+for i,j in enumerate(path_Bplus):
+        df=files(j,path_Bplus,m[i],t[i])
+
+        plt.figure()
+        plt.suptitle('Comparativa dos momentos transversais das partículas do decaemento\n$B^{+}~\longrightarrow~K^{+}~(H^{0}~\longrightarrow~\mu^{+}~ \mu{-}) \quad m=%d~MeV \quad t=%d~ps$' %(m[i],t[i]))
+        ax1=plt.subplot(1,2,1)
+        ax1.hist(df['Kplus_PT'], bins=50, color='blue', label='$K^{+}_{PT}$')
+        ax1.legend(loc='best')
+        ax1.set_xlabel('$Energy~(MeV)$')
+        ax1.set_ylabel('$Pseudorapidity$')
+        ax1.minorticks_on()
+        ax2=plt.subplot(1,2,2)
+        ax2.hist(df['muplus_PT'], bins=50, color='blue', label='$\mu^{+}_{PT}$')
+        ax2.hist(df['muminus_PT'], bins=50, color='red', label='$\mu^{-}_{PT}$')
+        ax2.legend(loc='best')
+        ax2.set_xlabel('$Energy~(MeV)$')
+        ax2.set_ylabel('$Pseudorapidity$')
+        ax2.minorticks_on()
+        plt.subplots_adjust(wspace=.5)
+        plt.savefig('/home3/andres.novo/TFM/first/PT_plots/hist_'+ j +'.png')
