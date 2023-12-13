0564fabd659a1b30ef7949cbf94af2a02327bf23
andresnovo <andresnovor@gmail.com>
2023-12-14 00:22:47 +0100

    Updated

--- first/second.py
+++ first/second.py
@@ -6,33 +6,104 @@ import numpy as np
 #import vecutils as vu
 import awkward as ak
 
-if __name__ == "__main__":
+def files(path,p,m,t):
     root = '/scratch47/forAndres/tuples/simulation/2016/'
-    path_B0 = '11114066'; path_Bplus = '12113098'
-    f0 = ur.open(root + path_B0 + '/StrippingB2KpiX2MuMuDarkBosonLine.root:B2KpiX2MuMuDarkBoson/DecayTree')
-    f1 = ur.open(root + path_Bplus + '/StrippingB2KX2MuMuDarkBosonLine.root:B2KX2MuMuDarkBoson/DecayTree')
-    print(f1.keys())
-    #print(f.arrays(['muminus_PIDp'], cut = ['muminus_TRUTHID', 'muplus_TRUEID'], library = 'pd'))
-    M_B0 = f0.arrays(['B_M'], library = 'pd')
-    M_Bplus = f1.arrays(['B_M'], library = 'pd')
-    print(f0.arrays(['Kplus_TRUEID', 'piminus_TRUEID'], library = 'pd'))
+    if p==path_B0:
+        f    = ur.open(root + path + '/StrippingB2KpiX2MuMuDarkBosonLine.root:B2KpiX2MuMuDarkBoson/DecayTree')
+        data = f.arrays(['piminus_PT','Kplus_PT','muminus_PT','muplus_PT', 'piminus_P', 'Kplus_P', 'muminus_P', 'muplus_P', 'piminus_TRUEID', 'Kplus_TRUEID', 'muminus_TRUEID', 'muplus_TRUEID'], library='pd')
+        cuts = data.query('Kplus_TRUEID == 321' and 'piminus_TRUEID == -211' and 'muplus_TRUEID == -13' and 'muminus_TRUEID == +13')
+    else:
+        f    = ur.open(root + path + '/StrippingB2KX2MuMuDarkBosonLine.root:B2KX2MuMuDarkBoson/DecayTree')
+        data = f.arrays(['Kplus_PT','muminus_PT','muplus_PT', 'Kplus_P', 'muminus_P', 'muplus_P', 'Kplus_TRUEID', 'muminus_TRUEID', 'muplus_TRUEID'], library='pd')
+        cuts = data.query('Kplus_TRUEID == 321' and 'muplus_TRUEID == -13' and 'muminus_TRUEID == +13')
+    return data, cuts
+    
+def plotter(path, path2, particles, particles_cut, representations, m, t):
+    if path2 == path_B0:
+        plt.figure(figsize = (12,7))
+        plt.title('Comparativa da pseudorapidity das partículas do decaemento\n $B^{0} \longrightarrow (K^{*0}_{(892)} \longrightarrow K^{+}~\pi^{-}) (H^{0} \longrightarrow \mu^{+}~\mu^{-}) \quad m=%d~MeV \quad t=%d~ps$' %(m,t))
+        plt.hist(df_data[particles], bins=50, color='darkblue', alpha=.7, label=representations)
+        plt.legend(loc='best')
+        plt.xlabel('$Energy~(MeV)$')
+        plt.ylabel('$Pseudorapidity$')
+        plt.minorticks_on()
+        plt.savefig('/home3/andres.novo/TFM/first/cut_plots/hist_'+ path + '_' + particles + '.png')
+        plt.close()
 
-    plt.figure()
-    plt.title('Masa do mesón $B^{0}$')
-    plt.hist(M_B0, bins = 300)
-    plt.xlabel('$m~(MeV)$')
-    plt.ylabel('Probability')
-    plt.minorticks_on()
-    plt.savefig('/home3/andres.novo/TFM/first/B0_M.png')
+        plt.figure(figsize = (12,7))
+        plt.title('Comparativa con "truth match cuts" da pseudorapidity das partículas do decaemento\n $B^{0} \longrightarrow (K^{*0}_{(892)} \longrightarrow K^{+}~\pi^{-}) (H^{0} \longrightarrow \mu^{+}~\mu^{-}) \quad m=%d~MeV \quad t=%d~ps$' %(m,t))
+        plt.hist(df_cuts[particles_cut], bins=50, color='darkblue', alpha=.7, label=representations)
+        plt.legend(loc='best')
+        plt.xlabel('$Energy~(MeV)$')
+        plt.ylabel('$Pseudorapidity$')
+        plt.minorticks_on()
+        plt.savefig('/home3/andres.novo/TFM/first/cut_plots/hist_' + path + '_' + particles_cut + '_cut.png')
+        plt.close()
 
-    plt.figure()
-    plt.title('Masa do mesón $B^{+}$')
-    plt.hist(M_Bplus, bins = 300)
-    plt.xlabel('$m~(MeV)$')
-    plt.ylabel('Probability')
-    plt.minorticks_on()
-    plt.savefig('/home3/andres.novo/TFM/first/Bplus_M.png')
+    else:
+        plt.figure(figsize = (12,7))
+        plt.title('Comparativa da pseudorapidity das partículas do decaemento\n$B^{+}~\longrightarrow~K^{+}~(H^{0}~\longrightarrow~\mu^{+}~ \mu{-}) \quad m=%d~MeV \quad t=%d~ps$' %(m,t))
+        plt.hist(df_data[particles], bins=50, color='darkblue', alpha=.7, label=representations)
+        plt.legend(loc='best')
+        plt.xlabel('$Energy~(MeV)$')
+        plt.ylabel('$Pseudorapidity$')
+        plt.minorticks_on()
+        plt.savefig('/home3/andres.novo/TFM/first/cut_plots/hist_' + path + '_' + particles + '.png')
+        plt.close()
+
+        plt.figure(figsize = (12,7))
+        plt.title('Comparativa con "truth match cuts" da pseudorapidity das partículas do decaemento\n$B^{+}~\longrightarrow~K^{+}~(H^{0}~\longrightarrow~\mu^{+}~ \mu{-}) \quad m=%d~MeV \quad t=%d~ps$' %(m,t))
+        plt.hist(df_cuts[particles_cut], bins=50, color='darkblue', alpha=.7, label=representations)
+        plt.legend(loc='best')
+        plt.xlabel('$Energy~(MeV)$')
+        plt.ylabel('$Pseudorapidity$')
+        plt.minorticks_on()
+        plt.savefig('/home3/andres.novo/TFM/first/cut_plots/hist_' + path + '_' + particles_cut + '_cut.png')
+        plt.close()
+    return
 
 
+if __name__ == "__main__":
+    path_B0    = ['11114066','11114074','11114077']
+    path_Bplus = ['12113086','12113095','12113098']
+    m = [2000,250,4000]
+    t = [100,100,100]
 
+    for i,j in enumerate(path_B0):
+        df_data, df_cuts = files(j,path_B0,m[i],t[i])
 
+        df_data.eval('eta_piminus = arccosh(piminus_P / piminus_PT)', inplace = True)
+        df_data.eval('eta_Kplus = arccosh(Kplus_P / Kplus_PT)', inplace = True)
+        df_data.eval('eta_muplus = arccosh(muplus_P / muplus_PT)', inplace = True)
+        df_data.eval('eta_muminus = arccosh(muminus_P / muminus_PT)', inplace = True)
+
+        df_cuts.eval('eta_piminus_cut = arccosh(piminus_P / piminus_PT)', inplace = True)
+        df_cuts.eval('eta_Kplus_cut = arccosh(Kplus_P / Kplus_PT)', inplace = True)
+        df_cuts.eval('eta_muplus_cut = arccosh(muplus_P / muplus_PT)', inplace = True)
+        df_cuts.eval('eta_muminus_cut = arccosh(muminus_P / muminus_PT)', inplace = True)
+
+        particles       = ['eta_piminus', 'eta_Kplus', 'eta_muplus', 'eta_muminus']
+        particles_cut   = ['eta_piminus_cut', 'eta_Kplus_cut', 'eta_muplus_cut', 'eta_muminus_cut']
+        representations = ['$\eta(\pi^{-})$', '$\eta(K^{+})$', '$\eta(\mu^{+})$', '$\eta(\mu^{-})$']
+
+        [plotter(j, path_B0, k, l, n, m[i], t[i]) for k, l, n in zip(particles, particles_cut, representations)]
+    
+
+    for i,j in enumerate(path_Bplus):
+        df_data, df_cuts = files(j,path_Bplus,m[i],t[i])
+
+        df_data.eval('eta_Kplus = arccosh(Kplus_P / Kplus_PT)', inplace = True)
+        df_data.eval('eta_muplus = arccosh(muplus_P / muplus_PT)', inplace = True)
+        df_data.eval('eta_muminus = arccosh(muminus_P / muminus_PT)', inplace = True)
+
+        df_cuts.eval('eta_Kplus_cut = arccosh(Kplus_P / Kplus_PT)', inplace = True)
+        df_cuts.eval('eta_muplus_cut = arccosh(muplus_P / muplus_PT)', inplace = True)
+        df_cuts.eval('eta_muminus_cut = arccosh(muminus_P / muminus_PT)', inplace = True)
+
+        particles       = ['eta_Kplus', 'eta_muplus', 'eta_muminus']
+        particles_cut   = ['eta_Kplus_cut', 'eta_muplus_cut', 'eta_muminus_cut']
+        representations = ['$\eta(K^{+})$', '$\eta(\mu^{+})$', '$\eta(\mu^{-})$']
+
+        [plotter(j, path_Bplus, k, l, n, m[i], t[i]) for k, l, n in zip(particles, particles_cut, representations)]
+
+    
